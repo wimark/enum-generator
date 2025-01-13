15a027a2985ae4daffd223d7987a538afa406789
Gleb Malakhov <gmalakhov@wimark.com>
2025-01-13 23:49:36 +0500

    Add: fix assoc enums for mgo driver

--- main.go
+++ main.go
@@ -159,7 +159,7 @@ func (en *%type) SetBSON(v bson.Raw) error {
 		return nil
 	}
 	var data_raw, data_found = in["data"]
-	if bytes.Equal(data_raw.Value, []byte("null")) {
+	if bytes.Equal(data_raw.Data, []byte("null")) {
 		data_found = false
 	}
 	var t %constr
